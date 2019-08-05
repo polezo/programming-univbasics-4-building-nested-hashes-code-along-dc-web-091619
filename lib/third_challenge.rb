e29de63856dcfc30428ed9624da15437f83e80b3
Adam Finch <atyreefinch@gmail.com>
2019-08-05 01:46:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,21 +10,25 @@ def third_challenge
         age:"54"
         },
       :hero => {
-         name: "Rome",
-        age:"15"
+         name: "Romeo",
+        age:"15",
         status:"alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet",
+        age:"50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet",
+        age:"51"
         },
       :heroine => {
-        #your key/value pairs here
+       name: "Juliet",
+        age:"15",
+        status:"alive"
         },
       :heroine_friends => []
    }
