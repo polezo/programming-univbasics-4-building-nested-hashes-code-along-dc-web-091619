ad592215ce3e200084aea96e4a45e979db90ff98
Adam Finch <atyreefinch@gmail.com>
2019-08-05 01:41:51 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,8 +6,11 @@ def second_challenge
       hero:{},
       hero_friends:[]
       }, 
-    :capulet => {
-      #your key/value pairs here!
+   :capulet => {
+      patriarch:{},
+      matriarch:{},
+      heroine:{},
+      heroine_friends:[]
       }}
 
   
